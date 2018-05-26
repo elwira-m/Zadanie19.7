5e4e7a59ce129e168554bd7ac896d7528a475353
Elwira Marcinkowska <elwira.marcinkowska@hotmail.com>
2018-05-27 00:01:24 +0200

    fix errors

--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -1,9 +1,15 @@
 import {connect} from 'react-redux';
 import Comment from './Comment';
 import {thumbUpComment} from './action'
+import {thumbDownComment} from './action'
 
 const mapDispatchToProps = dispatch => ({
-  thumbUpComment: (id) => dispatch(thumbUpComment(id))
+  thumbUpComment: (id) => dispatch(thumbUpComment(id)),
+  thumbDownComment: (id) => dispatch(thumbDownComment(id))
 });
+
+//const mapDispatchToProps = dispatch => ({
+//  thumbDownComment: (id) => dispatch(thumbDownComment(id))
+//});
 
 export default connect(null, mapDispatchToProps)(Comment);
